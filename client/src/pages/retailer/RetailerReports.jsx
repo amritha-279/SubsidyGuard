b16c3a985a16ffeeea5635062969bef009e66c2e
Amritha <amrithasekar27@gmail.com>
2026-07-15 09:04:57 +0530

    changed csv file download

--- client/src/pages/retailer/RetailerReports.jsx
+++ client/src/pages/retailer/RetailerReports.jsx
@@ -19,10 +19,14 @@ function toCSV(rows) {
 }
 
 function downloadCSV(csv, filename) {
-  const blob = new Blob([csv], { type: 'text/csv' });
+  const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
-  a.href = url; a.download = filename; a.click();
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
   URL.revokeObjectURL(url);
 }
 
